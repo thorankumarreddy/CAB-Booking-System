f7d4cc2b95d85f0ef785d051c2f7d6aba8830ada
Pramod Nanduri <pramodnanduri@ymail.com>
2023-05-14 20:04:46 -0700

    This is my  first change manjula github

---
+++ src/main/java/com/cab/booking/cab/CabApplication.java
@@ -0,0 +1,13 @@
+package com.cab.booking.cab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabApplication.class, args);
+	}
+
+}
